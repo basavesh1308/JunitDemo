19fa8cd09bdb8d0be241d8c747cd47816df24788
Basavesh <basavesh1308@gmail.com>
2021-01-09 18:02:02 +0530

    first master commit

---
+++ src/main/java/StringHelper.java
@@ -0,0 +1,12 @@
+public class StringHelper
+{
+    private String name;
+
+    public StringHelper(String name) {
+        this.name = name;
+    }
+
+    public int findLength(){
+        return name.length();
+    }
+}
---
+++ src/test/java/StringHelperTest.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringHelperTest {
+
+    private StringHelper stringHelper;
+
+    @Before
+    public void setup(){
+
+        stringHelper = new StringHelper("Basavesh");
+
+    }
+
+    @Test
+    public void testStringLength(){
+
+        int result = stringHelper.findLength();
+
+        Assert.assertEquals(9,result);
+
+    }
+}
